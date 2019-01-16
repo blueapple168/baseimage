1fc95e2d86dc44ca35634e4188f73422eeceda35
blueapple168 <blueapple1120@qq.com>
2019-01-16 09:43:44 +0800

    set debug default port to 8301

--- java_auto_heap/run-java.sh
+++ java_auto_heap/run-java.sh
@@ -236,7 +236,7 @@ run_java_options() {
 
 debug_options() {
   if [ -n "${JAVA_ENABLE_DEBUG:-}" ] || [ -n "${JAVA_DEBUG_ENABLE:-}" ] ||  [ -n "${JAVA_DEBUG:-}" ]; then
-    local debug_port="${JAVA_DEBUG_PORT:-5005}"
+    local debug_port="${JAVA_DEBUG_PORT:-8301}"
     local suspend_mode="n"
     if [ -n "${JAVA_DEBUG_SUSPEND:-}" ]; then
       if ! echo "${JAVA_DEBUG_SUSPEND}" | grep -q -e '^\(false\|n\|no\|0\)$'; then
